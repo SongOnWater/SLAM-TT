5b361a669f6a123c21f33c19f22074c035794ea0
Dylan Lu <dylanelu@gmail.com>
2024-11-24 23:21:01 -0800

    readme

--- TTNet/src/get_bounces.py
+++ TTNet/src/get_bounces.py
@@ -211,7 +211,7 @@ def process_video(configs):
 
 if __name__ == '__main__':
     configs = parse_configs()
-    configs.video_path = "demo_video_short.mp4"
+    configs.video_path = "demo_video_longer.mp4"
     configs.gpu_idx = 0
     configs.pretrained_path = "../checkpoints/ttnet.pth"
     configs.show_image = False
